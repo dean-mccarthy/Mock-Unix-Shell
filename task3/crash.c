c4645bfbdf950e40a2b9a15b5c3de419301aea8e
Dean McCarthy <deancm@castor.ece.ubc.ca>
2024-03-24 20:49:04 -0700

    works, has issues with concurrency

--- task3/crash.c
+++ task3/crash.c
@@ -4,19 +4,233 @@
 #include <unistd.h>
 #include <string.h>
 #include <assert.h>
+#include <signal.h>
 
 #define MAXLINE 1024
+#define MAXJOBS 65536
+#define KILLED 2
+#define RUNNING 1
+#define FINISHED 0
+#define SUSPENDED -1
+
+typedef struct {
+    pid_t PID;
+    int jobNum;
+    int status; //1:running, 0:finished, -1:suspended 2:killed
+    const char* name;
+    bool valid;
+} job;
+
+
+static int currJob = 1;
+static job *jobList[MAXJOBS];
+
+static int getJobNum(int PID) {
+    for (size_t i = currJob; i > 0; i--)
+    {
+        job *jobSearch = jobList[i];
+        if (jobSearch && jobSearch->PID == PID) {
+            return jobSearch->jobNum;
+        }
+    }
+    return -1;
+    
+}
+
+static void quit(const char **toks) {
+    if (toks[1] != NULL) {
+            const char *msg = "ERROR: quit takes no arguments\n";
+            write(STDERR_FILENO, msg, strlen(msg));
+    } else {
+            exit(0);
+    }
+}
+
+static void printJob(int i) {
+    job *currJob = jobList[i];
+        const char *status;
+        switch(currJob->status) {
+            case 1:
+                status = "running";
+                break;
+            case 0:
+                status = "finished";
+                break;
+            case -1:
+                status = "suspended";
+                break;
+            case 2:
+                status = "killed";
+                break;    
+            default:
+                status = NULL;
+        }
+        const char* msg = (char*)malloc(MAXLINE);
+        snprintf(msg, MAXLINE, "[%d] (%d)   %s  %s\n", i, currJob->PID, status, currJob->name);
+        write(STDOUT_FILENO, msg, strlen(msg));
+}
+
+static void jobs() {
+    for (size_t i = 1; i < currJob; i++)
+    {
+        if (jobList[i]->valid)
+        {
+            printJob(i);
+        }
+    }
+}
+
+static void nuke(const char **toks) {
+    if (toks[1] == NULL) {
+        //KILL all
+        for (size_t i = 1; i < currJob; i++)
+        {
+            job *killJob = jobList[i];
+            if (killJob && killJob->status == 1)
+            {
+                killJob->status = 2;
+                kill(killJob->PID, SIGKILL);
+            }
+            
+        }
+        
+    } else {
+        int i = 1;
+        while (toks[i] != NULL)
+        {
+            char * process = toks[i];
+            if (process[0] == '%')
+            {
+                int jobNumKill = 0;
+                int numReader = 1;
+                while (process[numReader] != '\0')
+                {
+                    jobNumKill = jobNumKill*10 + (process[numReader] - '0');
+                    numReader++;
+                }
+                
+                job * killJob = jobList[jobNumKill];
+                if (killJob && killJob->status == 1)
+                {
+                    killJob->status = 2;
+                    pid_t curPID = killJob->PID;
+                    kill(killJob->PID, SIGKILL);
+                }
+            } else {
+                //KILL process iff shell has not exited
+                int killPID = toks[i];
+                int jobNumKill = getJobNum(killPID) != -1
+                if (jobNumKill != -1)
+                {
+                    job * killJob = jobList[jobNumKill];
+                    if (killJob && killJob->status == 1)
+                    {
+                        killJob->status = 2;
+                        pid_t curPID = killJob->PID;
+                        kill(killJob->PID, SIGKILL);
+                    }
+                }
+                
+            }
+            i++;
+        }
+    }
+}
+
+static void foreground(const char **toks) {
+    if (toks[1] == NULL) {
+        const char *msg = "ERROR: fg requires at least one argument\n";
+        write(STDERR_FILENO, msg, strlen(msg));
+    } else {
+        char *process = toks[1];
+        if (process[0] == '%')
+        {
+            //puts job %n in foreground
+        } else {
+            //puts process n in foreground
+        }
+    }
+}
+
+static void background(const char **toks) {
+    if (toks[1] == NULL) {
+        const char *msg = "ERROR: bg requires at least one argument\n";
+        write(STDERR_FILENO, msg, strlen(msg));
+    } else {
+        int i = 1;
+        while (toks[i] != NULL)
+        {
+            char *process = toks[1];
+            if (process[0] == '%')
+            {
+                //send job %n to background
+            } else {
+                //send job n to background
+            }
+            i++;
+        }
+    }
+}
+
+static void runProcess(const char **toks, bool bg) {
+    char *process = toks[0];
+    int i = 0;
+    char *args[MAXLINE] = {};
+    while (toks[i] != NULL)
+    {
+        args[i] = toks[i];
+        i++;
+    }  
+    args[i] = NULL;
+    if (bg) //using & signal
+    {
+        sigset_t mask;
+        sigemptyset(&mask);
+        sigaddset(&mask, SIGCHLD);
+        sigprocmask(SIG_BLOCK, &mask, NULL);
+        
+        pid_t child = fork();
+        if (child == 0) {
+            sigprocmask(SIG_UNBLOCK, &mask, NULL);
+            int run = execvp(toks[0], args);
+            if (run == -1)
+            {
+                printf("Function not found\n");
+                kill(getpid(), SIGKILL);
+            }
+        } else {
+            job childJob = {child, currJob++, RUNNING, toks[0], true};
+            jobList[currJob - 1] = &childJob;
+            printJob(currJob - 1);
+            sigprocmask(SIG_UNBLOCK, &mask, NULL);
+        }
+    } else {
+        int run = execvp(toks[0], args);
+        if (run == -1)
+        {
+            printf("FXN failed to run\n");
+
+        } else {
+            printf("FXN run\n"); //should never get here
+        }
+    }     
+}
 
 void eval(const char **toks, bool bg) { // bg is true iff command ended with &
     assert(toks);
     if (*toks == NULL) return;
     if (strcmp(toks[0], "quit") == 0) {
-        if (toks[1] != NULL) {
-            const char *msg = "ERROR: quit takes no arguments\n";
-            write(STDERR_FILENO, msg, strlen(msg));
-        } else {
-            exit(0);
-        }
+        quit(toks);
+    } else if (strcmp(toks[0], "jobs") == 0) {
+        jobs();
+    } else if (strcmp(toks[0], "nuke") == 0) {
+        nuke(toks);
+    } else if (strcmp(toks[0], "fg") == 0) {
+        foreground(toks);
+    } else if (strcmp(toks[0], "bg") == 0) {
+        background(toks);
+    } else {
+        runProcess(toks, bg);
     }
 }
 
@@ -69,6 +283,33 @@ int repl() {
     return 0;
 }
 
+static void handler(int num) {
+    sigset_t mask;
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGCHLD);
+    sigprocmask(SIG_BLOCK, &mask, NULL);
+
+    
+    pid_t pidOut;
+    int status;
+
+    while ((pidOut = waitpid(-1, &status, WNOHANG)) > 0) {
+        int jobNum = getJobNum(pidOut);
+        job *deadJob = jobList[jobNum];
+        deadJob->valid = false;
+        if (deadJob->status != KILLED) deadJob->status = FINISHED;
+        printJob(jobNum);
+    }
+    sigprocmask(SIG_UNBLOCK, &mask, NULL);
+
+}
+
 int main(int argc, char **argv) {
+    struct sigaction sigact;
+    sigact.sa_handler = handler;
+    sigact.sa_flags = SA_RESTART;
+    sigemptyset(&sigact.sa_mask);
+    sigaction(SIGCHLD, &sigact, NULL);
+    
     return repl();
 }
